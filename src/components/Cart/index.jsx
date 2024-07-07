892aa4896f9a23851a41809c1e5ec3c728776928
Kourosh <kouroshsharifineyestani@gmail.com>
2024-07-07 03:11:04 -0700

    feat: add clearCart and removeFromCart actions to Cart component

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -3,8 +3,8 @@ import { CartContext } from "../../contexts/CartContext";
 
 // Cart component displays the items in the cart
 const Cart = () => {
-    // Use the cart state from CartContext
-    const { cart } = useContext(CartContext);
+    // Use the cart state and actions from CartContext
+    const { cart, removeFromCart, clearCart } = useContext(CartContext);
 
     return (
         <div>
@@ -13,9 +13,13 @@ const Cart = () => {
                 {cart.map((product) => (
                     <li key={product.id}>
                         {product.name} - ${product.price} x {product.quantity}
+                        <button onClick={() => removeFromCart(product.id)}>
+                            Remove
+                        </button>
                     </li>
                 ))}
             </ul>
+            <button onClick={clearCart}>Clear Cart</button>
         </div>
     );
 };
